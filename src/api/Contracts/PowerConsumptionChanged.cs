afa4921f55d54b3017290cc8900f6cb894ed33ec
Gautier Boder <gboder@outlook.com>
2025-03-04 20:35:58 +0100

    added reporter

---
+++ src/api/Contracts/PowerConsumptionChanged.cs
@@ -0,0 +1,11 @@
+﻿using Coravel.Events.Interfaces;
+
+public class PowerConsumptionChanged : IEvent
+{
+    public double BeforeAActPower { get; set; }
+    public double BeforeBActPower { get; set; }
+    public double BeforeCActPower { get; set; }
+    public double AfterAActPower { get; set; }
+    public double AfterBActPower { get; set; }
+    public double AfterCActPower { get; set; }
+}
---
+++ src/api/shelly-recorder/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using shelly_connector;
+
+var builder = new HostBuilder();
+
+builder.ConfigureHostConfiguration(configHost =>
+{
+    configHost.AddEnvironmentVariables();
+});
+
+builder.ConfigureServices((hostContext, services) =>
+{
+    services.AddShellyConnector(hostContext.Configuration);
+});
+
+builder.UseConsoleLifetime();
+
+var app = builder.Build();
+
+await app.RunAsync();
